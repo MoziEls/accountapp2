eedb40a9cd740156ef3e8aad16080e66d9e54440
Your Name <you@example.com>
2018-10-31 16:09:44 +0000

    conncted to database/h2

--- src/main/java/qa/com/accountapp2/Account.java
+++ src/main/java/qa/com/accountapp2/Account.java
@@ -1,9 +1,21 @@
 package qa.com.accountapp2;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
 
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	Long id;
+	@Column(length=25)
 	String firstName;
+	@Column(length=25)
 	String lastName;
+	@Column(length=6)
 	String accountNumber;
 
 	public Account ( String getfirstName, String getlastName, String getaccountNumber ) {
